80a1100e95310a5f9ac19b5e8a0365bd6764b16a
Dion Singh <55777067+DionMS8@users.noreply.github.com>
2021-12-30 22:41:37 -0400

    Created a Main Component

---
+++ react-nft-application/src/components/Main.js
@@ -0,0 +1,19 @@
+// IMPORTING THE REACT LIBRARY
+import React from "react"
+// IMPORTING THE CSS FILE FOR MAIN COMPONENT
+import "./Main.css"
+
+
+const Main = () => {
+    return (
+       <div className="main">
+          
+       </div>
+    )
+}
+
+export default Main
+
+
+
+
