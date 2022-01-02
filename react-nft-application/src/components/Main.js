2744adea923423278adc5dcf96f9a1951612f705
Dion Singh <55777067+DionMS8@users.noreply.github.com>
2022-01-02 01:05:02 -0400

    Finished the Main Component Section

--- react-nft-application/src/components/Main.js
+++ react-nft-application/src/components/Main.js
@@ -20,51 +20,59 @@ const Main = ({ selectedPunk, punkListData }) => {
 
     return (
        <div className="main">
-          <div className="mainContent">
+            <div className="mainContent">
 
-              <div className="punkHighlight">
+                <div className="punkHighlight">
                   <div className="punkContainer">
                       <img className="selectedPunk" src={activePunk.image_original_url} alt="" />
                   </div>
-              </div>
+                </div>
 
-              <div className="punkDetails" style={{ color: "#fff"}}>
-                    <div className="title">{activePunk.name}</div>
-                    <span className="itemNumber">• #{activePunk.token_id}</span>
-              </div>
-
-              <div className="owner">
-
-                    <div className="ownerImageContainer">
-                        <img src={activePunk.owner.profile_img_url} alt=""/> 
+                <div className="punkDetails" style={{ color: "#fff"}}>
+                    
+                    <div className="title">
+                        {activePunk.name}
+                        <span className="itemNumber">• #{activePunk.token_id}</span>
                     </div>
 
-                    <div className="ownerDetails">
-                        
-                        <div className="ownerAddressAndHandle">
-                            <div>{activePunk.owner.address}</div>
-                            <div className="ownerHandle">@dionms8</div>
-                       </div>
+                    <div className="owner">
 
-                        <div className="ownerLink">
-                            <img src={instagramLogo} alt="" />
+                        <div className="ownerImageContainer">
+                            <img src={activePunk.owner.profile_img_url} alt=""/> 
                         </div>
-                        <div className="ownerLink">
-                            <img src={twitterLogo} alt="" />
-                        </div>
-                        <div className="ownerLink">
-                            <img src={moreIcon} alt="" />
+
+                        <div className="ownerDetails">
+
+                            <div className="ownerAddressAndHandle">
+                                <div>{activePunk.owner.address}</div>
+                                <div className="ownerHandle">@dionms8</div>
+                            </div>
+
+                            <div className="ownerLink">
+                                <img src={instagramLogo} alt="" />
+                            </div>
+
+                            <div className="ownerLink">
+                                <img src={twitterLogo} alt="" />
+                            </div>
+
+                            <div className="ownerLink">
+                                <img src={moreIcon} alt="" />
+                            </div>
+
                         </div>
 
                     </div>
-              </div>
-          </div>
-       </div>
+
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
 export default Main
-
 
 
 
